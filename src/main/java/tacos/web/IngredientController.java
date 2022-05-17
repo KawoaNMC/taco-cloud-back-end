6d31ac690fe13b00d14bb0107d3fea6a3f75bc15
Kawoa NMC <manhcuong0978481376@gmail.com>
2022-05-17 08:52:36 +0700

    add back end

---
+++ src/main/java/tacos/web/IngredientController.java
@@ -0,0 +1,59 @@
+package tacos.web;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.EntityLinks;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.extern.slf4j.Slf4j;
+import tacos.data.IngredientRepository;
+import tacos.Ingredient;
+
+@Slf4j
+@RestController
+@RequestMapping(path = "/ingredients", produces = "application/json")
+@CrossOrigin(origins = "*")
+public class IngredientController {
+	private IngredientRepository ingredientRepo;
+	@Autowired
+	EntityLinks entityLinks;
+	
+	public IngredientController(IngredientRepository ingredientRepo) {
+		this.ingredientRepo = ingredientRepo;
+	}
+	
+	@GetMapping
+	public Iterable<Ingredient> getAllIngredients() {
+	return ingredientRepo.findAll();
+	}
+
+	@GetMapping("/{id}")
+	public Ingredient ingredientById(@PathVariable("id") String id) {
+		Optional<Ingredient> optIngredient = ingredientRepo.findById(id);
+		if (optIngredient.isPresent()) {
+			return optIngredient.get();
+		}
+		return null;
+	}
+
+	@GetMapping("/add")
+	public String showAddForm(Model model) {
+		model.addAttribute("ingredient", new Ingredient(null, null, null));
+		return "addIngredient";
+	}
+
+	@PostMapping
+	public String addIngredient(Ingredient ingredient, Model model) {
+		ingredientRepo.save(ingredient);
+		model.addAttribute(ingredient);
+		log.info("Ingredient saved: " + ingredient);
+		return "addIngredientSuccess";
+	}
+}
